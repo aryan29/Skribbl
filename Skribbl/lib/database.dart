81a7c3d7af533a1f3024ff78c472061b70ab7f4c
aryan29 <nk28agra@gmail.com>
2020-11-05 02:00:29 +0530

    40-50% of basic game done

--- Skribbl/lib/database.dart
+++ Skribbl/lib/database.dart
@@ -1,10 +1,11 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'global.dart' as global;
 
 class FirestoreService {
   static FirebaseFirestore _db = FirebaseFirestore.instance;
   //Send Data
   static sendData(id, file) async {
-    await _db.collection('rooms').doc(id).set(file);
+    await _db.collection('rooms').doc(id).update({"value": file});
   }
 
   //Get Data
@@ -15,9 +16,17 @@ class FirestoreService {
 
   static createRoom(name) async {
     //Creating Room
-    print("COming to create room");
+    print("Coming to create room");
     var path = _db.collection('rooms').doc();
-    await path.set({"height": 176, "width": 360, "lines": []});
+    await path.set({
+      "value": {
+        "height": 176,
+        "width": 360,
+        "lines": [],
+      },
+      "user_id": [],
+      "current": 0
+    });
     print(path.id.toString());
     await addUserInRoom(path.id.toString(), name);
     return path.id.toString();
@@ -27,14 +36,26 @@ class FirestoreService {
   static Future<int> addUserInRoom(roomId, name) async {
     print("Coming to add in room");
     var z = await _db.collection('rooms').doc(roomId).get().then((doc) async {
-      print(doc.exists);
-      print(roomId);
       if (doc.exists) {
+        //Add this user in room and also add this to user_id
+        var snap = await _db.collection("rooms").doc(roomId).get();
+        List li = snap.data()['users_id'];
+        if (li.length != 0) {
+          li.add(li.last + 1);
+          global.key = li.last + 1;
+        } else {
+          li.add(1);
+          global.key = 1;
+        }
+
+        var d = await _db.collection("rooms").doc(roomId).get();
         await _db
             .collection('rooms')
             .doc(roomId)
             .collection("users")
-            .add({"name": name, "score": 0, "entrytime": DateTime.now()});
+            .doc(global.key.toString())
+            .set({"name": name, "score": 0, "entrytime": DateTime.now()});
+
         return 1;
       } else {
         print("here");
@@ -42,6 +63,38 @@ class FirestoreService {
       }
     });
     return z;
+  }
+
+  static removeUser(roomId, name) async {
+    var snap = await _db.collection("rooms").doc(roomId).get();
+    var data = snap.data();
+    data['users_id'].remove(global.key);
+    _db.collection("rooms").doc(roomId).set(data, SetOptions(merge: true));
+    //Delete the document from users
+    await _db
+        .collection("rooms")
+        .doc(roomId)
+        .collection("users")
+        .doc(global.key.toString())
+        .delete();
+  }
+
+  static nextChance() async {
+    //Having a next chance will also decide on which user whiteboard
+    //will be editale and word will be shown
+    var snap = await _db.collection("rooms").doc(global.roomid).get();
+    var data = snap.data();
+    data['current'] += 1;
+    _db
+        .collection("rooms")
+        .doc(global.roomid)
+        .set(data, SetOptions(merge: true));
+  }
+
+  static getCurrentData() async {
+    var snap = await _db.collection("rooms").doc(global.roomid).get();
+    var data = snap.data();
+    return data['users_id']['current'];
   }
 
   static sendMessege(roomId, name, value) async {
--- Skribbl/lib/global.dart
+++ Skribbl/lib/global.dart
@@ -2,3 +2,4 @@ library skribbl.global;
 
 String roomid = "3owUHLDLWyrhKLxhvWi6";
 String name = "Aryan";
+int key = 0;
